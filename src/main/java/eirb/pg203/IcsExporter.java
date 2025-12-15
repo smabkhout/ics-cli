f30521c7759428faf74374f375d57df77d716b63
Anas Benbaki <abenbaki@enseirb-matmeca.fr>
2025-12-15 04:41:08 +0100

    a

--- src/main/java/eirb/pg203/IcsExporter.java
+++ src/main/java/eirb/pg203/IcsExporter.java
@@ -25,33 +25,57 @@ public class IcsExporter implements Exporter {
 
         output.write("END:VCALENDAR\r\n");
     }
-
+    //ecrire les events
     private void writeEvent(Event event, Writer output) throws IOException{
-        output.write("BEGIN:VEVENT\r\n");
-        
-        output.write("DTSTAMP:" + event.getDtstamp() + "\r\n");
-        output.write("DTSTART:" + event.getDtstart() + "\r\n");
-        output.write("DTEND:" + event.getDtend() + "\r\n");
-        output.write("SUMMARY:" + event.getSummary() + "\r\n");
-        output.write("LOCATION:" + event.getLocation() + "\r\n");
-        output.write("DESCRIPTION:" + event.getDescription() + "\r\n");
-        output.write("UID:" + event.getUID() +"\r\n");    
-
-        
+        output.write("BEGIN:VEVENT\r\n");    
+        writeProperly(output, "DTSTAMP", event.getDtstamp());
+        writeProperly(output, "DTSTART", event.getDtstart());
+        writeProperly(output, "DTEND", event.getDtend());
+        writeProperly(output, "SUMMARY", event.getSummary());
+        writeProperly(output, "LOCATION", event.getLocation());
+        writeProperly(output, "DESCRIPTION", event.getDescription());
+        writeProperly(output, "UID", event.getUID());        
         output.write("END:VEVENT\r\n");
     }
 
+    //ecrire les todos
     private void writeTodo(Todos todo, Writer output) throws IOException {
         output.write("BEGIN:VTODO\r\n");
         
-        output.write("UID:" + todo.getUID() +"\r\n");
-        output.write("SUMMARY:" + todo.getSummary() + "\r\n");
-        output.write("LOCATION:" + todo.getLocation() + "\r\n");
-        output.write("PERCENT-COMPLETE:" + todo.getProgress() + "\r\n");
-        output.write("DUE;VALUE=DATE:" + todo.getDueDate() + "\r\n");
-        output.write("STATUS:" + todo.getStatus() + "\r\n");
-        output.write("DTSTAMP:" + todo.getDtstamp() + "\r\n");
+        writeProperly(output, "UID", todo.getUID());        
+        writeProperly(output, "SUMMARY", todo.getSummary());
+        writeProperly(output, "LOCATION", todo.getLocation());
+        writeProperly(output, "PERCENT-COMPLETE", todo.getProgress());        
+        writeProperly(output, "DUE;VALUE=DATE", todo.getDueDate());        
+        writeProperly(output, "STATUS", todo.getStatus());        
+        writeProperly(output, "DTSTAMP", todo.getDtstamp());
         
         output.write("END:VTODO\r\n");
+    }
+
+    //pour eviter des lignes de longueur sup à 75 (warning)
+    private void writeProperly(Writer output, String key, String value) throws IOException {
+        if (value == null || value.isEmpty()) return;
+
+        String fullLine = key + ":" + value;
+
+        if (fullLine.length() <= 75) {
+            output.write(fullLine + "\r\n");
+            return;
+        }
+
+        
+        output.write(fullLine.substring(0, 75) + "\r\n");
+
+        String remainder = fullLine.substring(75);
+
+        while (remainder.length() > 74) {
+            output.write(" " + remainder.substring(0, 74) + "\r\n");
+            remainder = remainder.substring(74);
+        }
+
+        if (!remainder.isEmpty()) {
+            output.write(" " + remainder + "\r\n");
+        }
     }
 }
